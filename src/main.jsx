b73b44c30dc5b58c60cd4fba0115cb24c61616ea
ender179 <kirilukvlad91@gmail.com>
2024-12-11 15:35:01 +0200

    git push

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles.css';
-import App from './components/app';
+import App from '.src/App';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
