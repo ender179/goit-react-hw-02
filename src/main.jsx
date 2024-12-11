6b35a8cad6d53bac08e3e37d431a5de320a96de9
ender179 <kirilukvlad91@gmail.com>
2024-12-11 15:03:18 +0200

    git push

--- src/main.jsx
+++ src/main.jsx
@@ -1,61 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import Feedback from './components/Feedback/Feedback';
-import Options from './components/Options/Options';
-import Notification from './components/Notification/Notification';
+import React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import './styles.css';
+import App from './App';
 
-const App = () => {
-  const [feedback, setFeedback] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  });
-
-  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
-  const positiveFeedback = totalFeedback
-    ? Math.round((feedback.good / totalFeedback) * 100)
-    : 0;
-
-  useEffect(() => {
-    const storedFeedback = JSON.parse(localStorage.getItem('feedback'));
-    if (storedFeedback) {
-      setFeedback(storedFeedback);
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem('feedback', JSON.stringify(feedback));
-  }, [feedback]);
-
-  const updateFeedback = (feedbackType) => {
-    setFeedback((prevFeedback) => ({
-      ...prevFeedback,
-      [feedbackType]: prevFeedback[feedbackType] + 1,
-    }));
-  };
-
-  const resetFeedback = () => {
-    setFeedback({ good: 0, neutral: 0, bad: 0 });
-  };
-
-  return (
-    <div>
-      <h1>Sip Happens Café</h1>
-      <p>Please leave your feedback about our service by selecting one of the options below.</p>
-      <Options onLeaveFeedback={updateFeedback} onReset={resetFeedback} totalFeedback={totalFeedback} />
-      {totalFeedback > 0 ? (
-        <Feedback
-          good={feedback.good}
-          neutral={feedback.neutral}
-          bad={feedback.bad}
-          totalFeedback={totalFeedback}
-          positiveFeedback={positiveFeedback}
-        />
-      ) : (
-        <Notification message="No feedback given" />
-      )}
-    </div>
-  );
-};
-
-export default App;
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
